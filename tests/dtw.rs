9e20591f75e9176aaa0b40aab227c475fd3380bc
nucccc <dan.dom.nuc@gmail.com>
2025-10-22 01:31:46 +0200

    additional tests for generic dtw

--- tests/dtw.rs
+++ tests/dtw.rs
@@ -54,6 +54,76 @@ fn test_dtw_euclidean() {
 }
 
 #[test]
+fn test_dtw_euclidean_vec_arr() {
+    let dtw0 = dtw(
+        &vec![[1.0, 2.0], [1.0, 4.0], [3.0, 1.0]],
+        &vec![[2.0, 5.0], [4.0, 2.0]],
+        DistMetric::Euclidean
+    ).unwrap();
+
+    assert_eq!(dtw0, 5.99070478491457);
+
+    let dtw3 = dtw(
+        &vec![[1.0, 2.0, 3.0, 6.0], [1.0, 4.0, 7.0, 9.0], [3.0, 1.0, -1.0, 2.0]],
+        &vec![[2.0, 5.0, -7.0, 4.0], [4.0, 2.0, 4.0, 2.0]],
+        DistMetric::Euclidean
+    ).unwrap();
+
+    assert_eq!(dtw3, 24.299380447914302);
+
+    let dtw4 = dtw(
+        &vec![[1.0, 2.0, 3.0, 6.0], [1.0, 4.0, 7.0, 9.0], [3.0, 1.0, -1.0, 2.0], [12.0, 15.0, 1.0, 1.0], [16.0, 2.0, 1.0, 1.0]],
+        &vec![[2.0, 5.0, -7.0, 4.0], [4.0, 2.0, 4.0, 2.0], [1.0, 2.0, 7.0, -1.0], [1.0, 2.0, 3.0, 4.0]],
+        DistMetric::Euclidean
+    ).unwrap();
+
+    assert_eq!(dtw4, 57.89253119304076);
+
+    let dtw5 = dtw(        
+        &vec![[2.0, 5.0, -7.0, 4.0], [4.0, 2.0, 4.0, 2.0], [1.0, 2.0, 7.0, -1.0], [1.0, 2.0, 3.0, 4.0]],
+        &vec![[1.0, 2.0, 3.0, 6.0], [1.0, 4.0, 7.0, 9.0], [3.0, 1.0, -1.0, 2.0], [12.0, 15.0, 1.0, 1.0], [16.0, 2.0, 1.0, 1.0]],
+        DistMetric::Euclidean
+    ).unwrap();
+
+    assert_eq!(dtw5, 57.89253119304076);
+}
+
+#[test]
+fn test_dtw_euclidean_vec_vec() {
+    let dtw0 = dtw(
+        &vec![vec![1.0, 2.0], vec![1.0, 4.0], vec![3.0, 1.0]],
+        &vec![vec![2.0, 5.0], vec![4.0, 2.0]],
+        DistMetric::Euclidean
+    ).unwrap();
+
+    assert_eq!(dtw0, 5.99070478491457);
+
+    let dtw3 = dtw(
+        &vec![vec![1.0, 2.0, 3.0, 6.0], vec![1.0, 4.0, 7.0, 9.0], vec![3.0, 1.0, -1.0, 2.0]],
+        &vec![vec![2.0, 5.0, -7.0, 4.0], vec![4.0, 2.0, 4.0, 2.0]],
+        DistMetric::Euclidean
+    ).unwrap();
+
+    assert_eq!(dtw3, 24.299380447914302);
+
+    let dtw4 = dtw(
+        &vec![vec![1.0, 2.0, 3.0, 6.0], vec![1.0, 4.0, 7.0, 9.0], vec![3.0, 1.0, -1.0, 2.0], vec![12.0, 15.0, 1.0, 1.0], vec![16.0, 2.0, 1.0, 1.0]],
+        &vec![vec![2.0, 5.0, -7.0, 4.0], vec![4.0, 2.0, 4.0, 2.0], vec![1.0, 2.0, 7.0, -1.0], vec![1.0, 2.0, 3.0, 4.0]],
+        DistMetric::Euclidean
+    ).unwrap();
+
+    assert_eq!(dtw4, 57.89253119304076);
+
+    let dtw5 = dtw(        
+        &vec![vec![2.0, 5.0, -7.0, 4.0], vec![4.0, 2.0, 4.0, 2.0], vec![1.0, 2.0, 7.0, -1.0], vec![1.0, 2.0, 3.0, 4.0]],
+        &vec![vec![1.0, 2.0, 3.0, 6.0], vec![1.0, 4.0, 7.0, 9.0], vec![3.0, 1.0, -1.0, 2.0], vec![12.0, 15.0, 1.0, 1.0], vec![16.0, 2.0, 1.0, 1.0]],
+        DistMetric::Euclidean
+    ).unwrap();
+
+    assert_eq!(dtw5, 57.89253119304076);
+}
+
+#[test]
 fn test_dtw_euclidean_single_dim_array() {
     let dtw0 = dtw(
         &array![1.0, 1.0, 3.0],
@@ -70,6 +140,44 @@ fn test_dtw_euclidean_single_dim_array() {
     ).unwrap();
 
     assert_eq!(dtw1, 5.3);
+}
+
+#[test]
+fn test_dtw_euclidean_single_dim_vec() {
+    let dtw0 = dtw(
+        &vec![1.0, 1.0, 3.0],
+        &vec![2.0, 4.0],
+        DistMetric::Euclidean
+    ).unwrap();
+
+    assert_eq!(dtw0, 3.0);
+
+    let dtw1 = dtw(
+        &vec![1.0, 3.0, 4.0],
+        &vec![1.0, 7.3],
+        DistMetric::Euclidean
+    ).unwrap();
+
+    assert_eq!(dtw1, 5.3);
+}
+
+#[test]
+fn test_dtw_euclidean_single_dim_vec_f32() {
+    let dtw0 = dtw(
+        &vec![1.0_f32, 1.0, 3.0],
+        &vec![2.0_f32, 4.0],
+        DistMetric::Euclidean
+    ).unwrap();
+
+    assert_eq!(dtw0, 3.0);
+
+    let dtw1 = dtw(
+        &vec![1.0_f32, 3.0, 4.0],
+        &vec![1.0_f32, 7.3],
+        DistMetric::Euclidean
+    ).unwrap();
+
+    assert_eq!(dtw1, 5.300000190734863);
 }
 
 #[test]
@@ -117,6 +225,18 @@ fn test_dtw_manhattan() {
     ).unwrap();
 
     assert_eq!(dtw1, 5.3);
+}
+
+#[test]
+fn test_dtw_euclidean_vec_vec_err() {
+    // just to ensure that if a subvector has a different len an error is returned
+    let res = dtw(
+        &vec![vec![1.0, 2.0], vec![1.0, 4.0], vec![3.0, 1.0]],
+        &vec![vec![2.0, 5.0], vec![4.0, 2.0, 5.0]],
+        DistMetric::Euclidean
+    );
+
+    assert!(res.is_err());
 }
 
 #[test]
